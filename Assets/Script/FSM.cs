50a8dc0c315d08d465547e054d6cd43d2b328cfd
SardineFish <Sardine@live.cn>
2018-07-07 11:10:56 +0800

    Add FSM

---
+++ Assets/Script/FSM.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FSM : MonoBehaviour {
+
+    State currentState;
+    public State CurrentState
+    {
+        get { return currentState; }
+        set { ChangeState(value); }
+    }
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        CurrentState.OnUpdate();
+	}
+
+    public void ChangeState(State nextState)
+    {
+        if (CurrentState != null && CurrentState.OnExit(nextState))
+        {
+            if (nextState.OnEnter(CurrentState))
+            {
+                currentState = nextState;
+            }
+        }
+    }
+
+    public void ForceChangeState(State state)
+    {
+        CurrentState?.OnExit(state);
+        state.OnEnter(CurrentState);
+        currentState = state;
+    }
+}
---
+++ Assets/Script/State.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public abstract class State
+{
+    public virtual bool OnEnter(State previousState) => true;
+
+    public virtual bool OnExit(State nextState) => true;
+
+    public virtual void OnUpdate() { }
+}
